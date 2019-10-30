e47bf8eed6833220f3e9702a3a4b1beecb7b660a
Ben Clark <bclark@sudoblark.com>
2019-10-30 16:27:09 +0000

    Drafted enemy classes

---
+++ Butterchase/LevelClasses/Level.py
@@ -0,0 +1,20 @@
+from PlayerClasses import Player
+
+# Level base
+class Level:
+    # Constructor
+    def __init__(self, player):
+        self.player = player
+        self.roomList = 1,2,3
+        self.enemyList = []
+
+    # Method to generate list of enemies for the level
+    def populateEnemyList(self):
+        pass
+
+
+
+
+
+        
+
