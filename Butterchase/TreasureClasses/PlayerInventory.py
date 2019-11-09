b5d943114ac0fa4eb3a20a4d47d4e5842211a44c
Benjamin Clark <bclark@sudoblark.com>
2019-11-09 16:28:03 +0000

    Create PlayerInventory.py
    - Drafted pseudo-logic for player inventory class
    - Logic will show player inventory but not allow them to equip etc, this will be added in later

---
+++ Butterchase/TreasureClasses/PlayerInventory.py
@@ -0,0 +1,13 @@
+class PlayerInventory():
+    def OutputInventory(self, player):
+        pass
+    def OutputWeapons(self, weaponList):
+        # sword (+1 attack)   spear (+2 attack)
+        # blah (+1 attack - equipped)
+        pass
+    def OutputArmour(self, armourList):
+        # leather armour (+1 defence)
+        # cloth (+1 defence)  bed pan (+0 defence - equipped)
+
+
+    #OUTPUT A TABLE WITH STATS SHOWN - USE SAME SORT OF LOGIC AS SHOWMAP
