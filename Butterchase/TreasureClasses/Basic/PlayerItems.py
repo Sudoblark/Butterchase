6fb9debe0edee48b28103bebf7d8db5ff6fa67ec
Ben Clark <bclark@sudoblark.com>
2019-11-14 16:58:34 +0000

    Update PlayerItems.py
    - Updated BasicShield and WoodenClub

--- Butterchase/TreasureClasses/Basic/PlayerItems.py
+++ Butterchase/TreasureClasses/Basic/PlayerItems.py
@@ -39,22 +39,31 @@ class IdioticSword(Treasure):
         msg1 = "{0} impales {1} on their {2}. {0} pulls their {2} out of {1}'s now lifeless body.".format(self.player.name, enemy.name, self.name)
         msg2 = "{1} attempts to grab {0}'s weapon. It's rusty, dirty, contaminated... Pathogens enter {1}'s blood stream, destroy their immune system, and cause {1} to instantly die from accute too-many-crisps syndrome.".format(self.player.name, enemy.name)
         msg3 = "{0} parry's an attack from {1}'s {2}, twirls round and slices through the {3} protecting {1}'s vital organs. {1} slumps to the floor, limp and unmoving...".format(self.player.name, enemy.name, enemy.weapon.name, enemy.armour.name)
-        msgArray = [msg, msg1, msg2]
+        msgArray = [msg, msg1, msg2, msg3]
         print(GetRandomMessage(msgArray))
 class WoodenClub(Treasure):
     def __init__(self):
         Treasure.__init__(self, TreasureTypes.Weapon, 4, 0, "Wooden Club")
     def attackFlavour(self, enemy):
-        msg = "{0} wacks {1} upside the head. {0} isn't sure if the woodemn thud comes from {1} or {2}".format(self.player.name, enemy.name, self.name)
-        print(msg)
+        msg = "{0} wacks {1} upside the head. {0} isn't sure if the wooden thud comes from {1} or {2}".format(self.player.name, enemy.name, self.name)
+        msg1 = "{1} is knock knocking knock on {0}'s head... ouch that must hurt".format(enemy.name, self.player.name)
+        msg2 = "{0} ducks low, and manages to aggressively bring their {1} into contact with {2}'s left kneecap... ow".format(self.player.name, self.name, enemy.name)
+        msg3 = "SNAP! That's the sound of the metacarpal bones in {0}'s hand being shattered by {1}'s {2}".format(enemy.name, self.player.name, self.name)
+        msgArray = [msg, msg1, msg2, msg3]
+        print(GetRandomMessage(msgArray))
     def executionText(self, enemy):
-        msg = "{0} clumps {1} with {2}, turning their brain to mush".format(self.player.name, enemy.name, self.name)
-        print(msg)
+        msg = "{0} violently clumps {1} with {2}, turning their brain to mush".format(self.player.name, enemy.name, self.name)
+        msg1 = "{0} howls in an angry bloodlust, feverishly reigning blows down upon {1}'s body until all that's left of their face is a gooey, unrecognisable, mess".format(self.player.name, enemy.name)
+        msg2 = "{0} strikes {1}'s {2} with such force that it's no longer usable. Defenceless, {1} tries to grab the club but it's too late; {0} aggressively shatters their hands. {1}'s {3} holds, but their internal organs do not. They slump to the floor and do get up.".format(self.player.name, enemy.name, enemy.weapon.name, enemy.armour.name)
+        msg3 = "{0} grunts 'me club {1}'. {0} clubs {1}, {0} clubs {1} 15 times until {1} stops moving".format(self.player.name, enemy.name)
+        msgArray = [msg, msg1, msg2, msg3]
+        print(GetRandomMessage(msgArray))
 class Seax(Treasure):
     def __init__(self):
         Treasure.__init__(self, TreasureTypes.Weapon, 5, 0, "Seax")
 class Spear(Treasure):
-    pass
+    def __init__(self):
+        Treasure.__init__(self, TreasureTypes.Weapon, 6, 0, "Spear")
 #endregion
 
 #region armour
@@ -67,7 +76,8 @@ class BasicShield(Treasure):
     def __init__(self):
         Treasure.__init__(self, TreasureTypes.Armour, 0, 3, "Scutum")
     def defenceFlavour(self,enemy):
-        pass
+        msg = "{0} manages to raise their Scutum just in time, catching {1}'s {2} and deflecting the damage".format(self.player.name, enemy.name, enemy.weapon.name)
+        print(msg)
 class LeatherArmour(Treasure):
     pass
 class StuddedLeatherArmour(Treasure):
@@ -78,5 +88,5 @@ class Doublet(Treasure):
 #endregion
 # Item arrays that get reference
 PlayerWeapons = [Hands(), IdioticSword(), WoodenClub()]
-PlayerArmour = [IdioticArmour()]
+PlayerArmour = [IdioticArmour(), BasicShield()]
 
